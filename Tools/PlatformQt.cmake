69bb87913b2866631961abe69da475c450fbe757
Konstantin Tokarev <annulen@yandex.ru>
2016-10-26 20:36:43 +0300

    WIP: Support compilation with MinGW
    Change-Id: I2151485cc5d55a68227f5d08911128f14f269ea0

--- Tools/PlatformQt.cmake
+++ Tools/PlatformQt.cmake
@@ -1,6 +1,7 @@
-add_subdirectory(DumpRenderTree)
-add_subdirectory(ImageDiff)
+add_subdirectory(QtTestBrowser)
+
 if (ENABLE_TEST_SUPPORT)
+    add_subdirectory(DumpRenderTree)
+    add_subdirectory(ImageDiff)
     add_subdirectory(TestWebKitAPI)
-    add_subdirectory(QtTestBrowser)
 endif ()
