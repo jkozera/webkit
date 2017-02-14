13dc9f8ba1b4e60e03458ae4ef1e42f4cbae93fc
Konstantin Tokarev <annulen@yandex.ru>
2017-02-14 22:39:06 +0300

    Fixed "Unhandled web process message" errors when launching MiniBrowser
    Changed order of objects initialization, so initializeWebPage() could
    be called before further WebPageProxy set up.
    
    See also https://bugs.webkit.org/show_bug.cgi?id=152048
    
    Change-Id: Idb946aa93a0823fde7d782dded9127869227d9d2

--- Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
+++ Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
@@ -355,9 +355,11 @@ void QQuickWebViewPrivate::initialize(WKPageConfigurationRef configurationRef)
     webPage = toAPI(webPageProxy.get());
     pageToView()->insert(webPage.get(), this);
 
-    webPageProxy->setUseFixedLayout(s_flickableViewportEnabled);
-
     pageEventHandler.reset(new QtWebPageEventHandler(webPage.get(), pageView.data(), q_ptr));
+    pageClient.initialize(q_ptr, pageEventHandler.data(), &undoController);
+    webPageProxy->initializeWebPage();
+
+    webPageProxy->setUseFixedLayout(s_flickableViewportEnabled);
 
     {
         WKPageFindClientV0 findClient;
@@ -408,8 +410,6 @@ void QQuickWebViewPrivate::initialize(WKPageConfigurationRef configurationRef)
     preferences.setWebGLEnabled(true);
     preferences.setForceCompositingMode(true);
 
-    pageClient.initialize(q_ptr, pageEventHandler.data(), &undoController);
-    webPageProxy->initializeWebPage();
     webPageProxy->registerApplicationScheme(ASCIILiteral("qrc"));
 
     q_ptr->setAcceptedMouseButtons(Qt::MouseButtonMask);
